dbee10e2609ae0b1c0b5938547295113179e0878
utq <celalcankaya@outlook.com>
2020-03-13 17:02:51 +0000

    Initial Commit

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
